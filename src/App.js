95a07c2c3f52cb3f9e9cd0928e4b4617fd63f17f
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-25 18:43:58 -0400

    starting with prop types and making first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // render is a life cycle method
+  render() {
+    return (
+      // JSX not HTML
+      <div className="App">
+        {/* Can pass props in like this:
+         <Navbar title="Github Finder" icon='fab fa-github'></Navbar>*/}
+        <Navbar></Navbar>
+        <h1>Hello from react</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
