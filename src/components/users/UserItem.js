b7b8738bae60967b7be531309c3ea2e338c67953
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-25 21:35:01 -0400

    converted UserItem from class to function

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
 
-class UserItem extends Component {
+const UserItem = (props) => {
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
+    const { login, avatar_url, html_url } = props.user;
 
-        return (
-            <div className= "card text-center">
-                <img src={avatar_url} alt="" className ="round-img" style={{width: '60px'}}/>
-                <h3>{login}</h3>
-                <div>
-                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+    return (
+        <div className= "card text-center">
+            <img src={avatar_url} alt="" className ="round-img" style={{width: '60px'}}/>
+            <h3>{login}</h3>
+            <div>
+                <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 export default UserItem
