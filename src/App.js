dc91040eb4032810bdc6710df20ac001d4a03ea3
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-10-25 21:51:14 -0400

    using axios to pull data from github users

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Navbar from './components/layout/Navbar';
 // import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  // lifestyle method
+  componentDidMount() {
+    axios.get('https://api.github.com/users').then(res => console.log(res.data));
+  }
 
   // render is a life cycle method
   render() {
