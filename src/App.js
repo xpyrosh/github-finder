b648059394b94130d784bd5ed9e30885c910d948
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-11-30 19:41:39 -0500

    convert app.js to functional instead of class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 // import UserItem from './components/users/UserItem';
@@ -11,106 +11,114 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import './App.css';
 
-class App extends React.Component {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null,
-  }
+  // Same as above ^
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showUsers: PropTypes.bool.isRequired
-  };
+  // state = {
+  //   users: [],
+  //   user: {},
+  //   repos: [],
+  //   loading: false,
+  //   alert: null,
+  // }
+
+
 
   // lifestyle method
-  async componentDidMount() {
-    this.setState({loading: true});
+  // async componentDidMount() {
+  //   this.setState({loading: true});
 
-    const res = await axios.get(`https://api.github.com/users?&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   const res = await axios.get(`https://api.github.com/users?&client_id${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
-    this.setState({users:res.data, loading:false});
-  }
+  //   this.setState({users:res.data, loading:false});
+  // };
 
   // Search github users
-  searchUsers = async text => {
-    this.setState({loading: true});
+  const searchUsers = async text => {
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ users: res.data.items, loading:false });
+    setUsers(res.data.items);
+    setLoading(false);
   };
 
   // Get single user
-  getUser = async (username) => {
-    this.setState({loading: true});
+  const getUser = async (username) => {
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ user: res.data, loading:false });
+    setUser(res.data);
+    setLoading(false);
   }; 
  
   // Get user repos
-  getUserRepos = async (username) => {
-    this.setState({loading: true});
+  const getUserRepos = async (username) => {
+    setLoading(true);
 
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
-    this.setState({ repos: res.data, loading:false });
+    setRepos(res.data);
+    setLoading(false);
   }; 
 
   // Clear users from state
-  clearUsers = () => this.setState({
-    users:[],
-    loading: false
-  });
-
-  // Set Alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: {msg: msg, type: type }});
-
-    setTimeout(() => this.setState({alert: null}), 5000);
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   };
 
-  // render is a life cycle method
-  render() {
-    const { users, loading, user, repos } = this.state;
+  // Set Alert
+  const showAlert = (msg, type) => {
+    setAlert({msg, type});
 
-    return (
-      <Router>
-        {/* // JSX not HTML */}
-        <div className="App">
-          {/* Can pass props in like this:
-          <Navbar title="Github Finder" icon='fab fa-github'></Navbar>*/}
-          <Navbar/>
-          <div className="container">
-            <Alert alert={this.state.alert}/>
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search 
-                    searchUsers={this.searchUsers} 
-                    clearUsers={this.clearUsers} 
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={this.setAlert}
-                  />
-                  <Users loading={loading} users={users}/>
-                </Fragment>
-              )}/>
-              <Route exact path='/about' component={About}/>
-              <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={this.getUser} user={user} loading={loading} getUserRepos={this.getUserRepos} repos={repos}/>
-              )}/>
-            </Switch>
-          </div>
+    setTimeout(() => setAlert(null), 5000);
+  };
+
+  return (
+    <Router>
+      {/* // JSX not HTML */}
+      <div className="App">
+        {/* Can pass props in like this:
+        <Navbar title="Github Finder" icon='fab fa-github'></Navbar>*/}
+        <Navbar/>
+        <div className="container">
+          <Alert alert={alert}/>
+          <Switch>
+            <Route exact path='/' render={props => (
+              <Fragment>
+                <Search 
+                  searchUsers={searchUsers} 
+                  clearUsers={clearUsers} 
+                  showClear={users.length > 0 ? true : false}
+                  setAlert={showAlert}
+                />
+                <Users loading={loading} users={users}/>
+              </Fragment>
+            )}/>
+            <Route exact path='/about' component={About}/>
+            <Route exact path='/user/:login' render={props => (
+              <User {...props} getUser={getUser} user={user} loading={loading} getUserRepos={getUserRepos} repos={repos}/>
+            )}/>
+          </Switch>
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
+  
 }
+
+App.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showUsers: PropTypes.bool.isRequired
+};
 
 export default App;
