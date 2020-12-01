e9575b3ccf4f955e963f5f0cc1eeceb914ca1493
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-11-30 21:29:34 -0500

    added 404 page component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Alert from './components/layout/Alert';
 
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import PropTypes from 'prop-types';
 
@@ -31,6 +32,7 @@ const App = () => {
                 <Route exact path='/' component={Home}/>
                 <Route exact path='/about' component={About}/>
                 <Route exact path='/user/:login' component={User}/>
+                <Route component={NotFound}/>
               </Switch>
             </div>
           </div>
